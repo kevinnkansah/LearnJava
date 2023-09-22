c6c964a3461081a1fb59b865f8d598c0ccf756b3
kevinnkansah <kevinnkansah6@gmail.com>
2023-09-22 14:27:24 +0200

    commit

--- src/Practical.java
+++ src/Practical.java
@@ -1,6 +1,23 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+
+
 public class Practical {
 
+    String fileArray[] = {
+     "cipher1.txt",
+     "cipher2.txt",
+     "cipher3.txt",
+     "cipher4.txt",
+     "cipher5.txt",
+     "cipher6.txt",
+     "cipher7.txt"};
 
+     for (String file : fileArray) {
+
+     }
 
 
 
